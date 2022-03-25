215442bc08d7ab58ce4bdd1e847c66d21b3adb1b
EmoonX <gustavo.chicato@usp.br>
2022-03-24 22:16:22 -0300

    [01] Print chosen option

--- 01-higher-lower-heads-tails/heads-tails.go
+++ 01-higher-lower-heads-tails/heads-tails.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 func main() {
-	var input string
-	fmt.Print("Please choose Heads (h) or Tails (t): ")
-	fmt.Scanln(&input)
-	var option byte = input[0]
-	fmt.Printf("%c\n", option)
+	var option rune
+	for option != 'h' && option != 't' {
+		var input string
+		fmt.Print("Please choose Heads (h) or Tails (t): ")
+		fmt.Scanln(&input)
+		option = unicode.ToLower(rune(input[0]))
+	}
+	var s = "HEADS"
+	if option == 't' {
+		s = "TAILS"
+	}
+	fmt.Printf("> You chose %s.\n", s)
 }
