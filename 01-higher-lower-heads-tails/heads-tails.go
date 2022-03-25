5f960b2c70f4cb1df324df17f79c772e36fb4d29
EmoonX <gustavo.chicato@usp.br>
2022-03-24 22:45:43 -0300

    [01] Toss coin and print result

--- 01-higher-lower-heads-tails/heads-tails.go
+++ 01-higher-lower-heads-tails/heads-tails.go
@@ -2,20 +2,48 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 	"unicode"
 )
 
+// Randomly return either `"HEADS"` or `"TAILS"`, with equal probability.
+func CoinToss() string {
+	var n = rand.Int() % 2
+	if n == 0 {
+		return "HEADS"
+	}
+	return "TAILS"
+}
+
+// Print given number of `.`s, waiting 1 second after each.
+func PrintDotAndSleep(n uint) {
+	for i := uint(0); i < n; i++ {
+		fmt.Print(".")
+		time.Sleep(time.Second)
+	}
+}
+
 func main() {
-	var option rune
-	for option != 'h' && option != 't' {
+	// Read user input
+	var c rune
+	for c != 'h' && c != 't' {
 		var input string
 		fmt.Print("Please choose Heads (h) or Tails (t): ")
 		fmt.Scanln(&input)
-		option = unicode.ToLower(rune(input[0]))
+		c = unicode.ToLower(rune(input[0]))
 	}
-	var s = "HEADS"
-	if option == 't' {
-		s = "TAILS"
+	var option = "HEADS"
+	if c == 't' {
+		option = "TAILS"
 	}
-	fmt.Printf("> You chose %s.\n", s)
+	fmt.Printf("> You chose %s.\n", option)
+
+	// Toss coin and print result
+	var result = CoinToss()
+	fmt.Print("> The coin is tossed")
+	PrintDotAndSleep(3)
+	fmt.Printf("%s!", result)
+	time.Sleep(time.Second)
+	fmt.Println()
 }
