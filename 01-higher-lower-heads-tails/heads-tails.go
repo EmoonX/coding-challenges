a8626a1728a0f6b475b0a99dc285b5958d3f0daf
EmoonX <gustavo.chicato@usp.br>
2022-03-25 09:45:14 -0300

    [01] Show result (match or not)

--- 01-higher-lower-heads-tails/heads-tails.go
+++ 01-higher-lower-heads-tails/heads-tails.go
@@ -7,6 +7,21 @@ import (
 	"unicode"
 )
 
+// Show input dialog and scan user's input until
+// either `op1`` or `op2` are entered.
+//
+// Return user's answer.
+func GetAnswer(text string, op1 rune, op2 rune) rune {
+	var c rune
+	for c != op1 && c != op2 {
+		var input string
+		fmt.Printf("%s: ", text)
+		fmt.Scanln(&input)
+		c = unicode.ToLower(rune(input[0]))
+	}
+	return c
+}
+
 // Randomly return either `"HEADS"` or `"TAILS"`, with equal probability.
 func CoinToss() string {
 	var n = rand.Int() % 2
@@ -24,26 +39,35 @@ func PrintDotAndSleep(n uint) {
 	}
 }
 
-func main() {
-	// Read user input
-	var c rune
-	for c != 'h' && c != 't' {
-		var input string
-		fmt.Print("Please choose Heads (h) or Tails (t): ")
-		fmt.Scanln(&input)
-		c = unicode.ToLower(rune(input[0]))
-	}
+// Run main game loop.
+func Loop(points uint) {
+	// Get user's coin side choice
+	var text = "! Please choose Heads (h) or Tails (t)"
+	var c = GetAnswer(text, 'h', 't')
 	var option = "HEADS"
 	if c == 't' {
 		option = "TAILS"
 	}
-	fmt.Printf("> You chose %s.\n", option)
+	fmt.Printf("\n> You chose %s.\n", option)
 
 	// Toss coin and print result
-	var result = CoinToss()
+	var outcome = CoinToss()
 	fmt.Print("> The coin is tossed")
 	PrintDotAndSleep(3)
-	fmt.Printf("%s!", result)
+	fmt.Printf("%s!", outcome)
 	time.Sleep(time.Second)
+	fmt.Println()
+
+	// Show result (match or not)
+	if outcome == option {
+		fmt.Println("\n> You win!")
+		fmt.Printf("# Current score: %d\n", points)
+	} else {
+		fmt.Println("\n> You lose...")
+	}
+}
+
+func main() {
+	Loop(1)
 	fmt.Println()
 }
