8ffc8f1c92b6f53dbd73f64175db9636131d0aee
EmoonX <gustavo.chicato@usp.br>
2022-03-29 16:30:08 -0300

    [25] Generate and draw maze w/o cycles

--- 25-maze-generator-solver/Generator.cs
+++ 25-maze-generator-solver/Generator.cs
@@ -3,14 +3,56 @@ partial class Maze {
     public Maze(int m, int n) {
         this.m = m;
         this.n = n;
-
-        Random rand = new();
-        board = new Node[m, n];
+        board = new char[m, n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int k = rand.Next(2);
-                char c = (k == 0) ? ' ' : '█';
-                board[i, j] = new(c);
+                board[i, j] = WALL;
+            }
+        }
+        DrawBase();
+
+        board[1, 0] = EMPTY;
+        DrawOnCell(1, 0, EMPTY);
+        var (i0, j0) = (1, 1);
+        BuildGraph(i0, j0);
+        DrawOnCell(m+1, 0, '\n');
+    }
+
+    void BuildGraph(int i, int j) {
+        if (i == 0 || i == m-1 || j == 0 || j == n-1) {
+            return;
+        }
+        var countEmpty = (int i, int j) => (board[i, j] == EMPTY) ? 1 : 0;
+        int count = 0;
+        count += countEmpty(i  , j-1) + countEmpty(i,   j+1);
+        count += countEmpty(i-1, j  ) + countEmpty(i+1, j  );
+        if (count >= 2) {
+            return;
+        }
+        var diagonals = new [] {(-1, -1), (-1, +1), (+1, -1), (+1, +1)};
+        foreach (var diag in diagonals) {
+            var (iTo, jTo) = (i + diag.Item1, j + diag.Item2);
+            if (board[iTo, jTo] == WALL) {
+                continue;
+            }
+            count = 0;
+            count += countEmpty(i  , j  ) + countEmpty(iTo, jTo);
+            count += countEmpty(i  , jTo) + countEmpty(iTo, j  );
+            if (count == 1) {
+                return;
+            }
+        }
+        board[i, j] = EMPTY;
+        DrawOnCell(i, j, EMPTY);
+        Thread.Sleep(20);
+
+        Random rand = new();
+        var directions = new [] {(0, -1), (0, +1), (-1, 0), (+1, 0)};
+        directions = directions.OrderBy(x => rand.Next()).ToArray();
+        foreach (var dir in directions) {
+            var (iTo, jTo) = (i + dir.Item1, j + dir.Item2);
+            if (board[iTo, jTo] == WALL) {
+                BuildGraph(iTo, jTo);
             }
         }
     }
@@ -19,16 +61,8 @@ partial class Maze {
 class Generator {
     static void Main() {
 
-        int m = 20;
+        int m = 30;
         int n = 60;
         Maze maze = new(m, n);
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                char c = maze.board[i,j].c;
-                Console.Write(c);
-            }
-            Console.WriteLine();
-        }
     }
 }
--- 25-maze-generator-solver/Maze.cs
+++ 25-maze-generator-solver/Maze.cs
@@ -1,14 +1,40 @@
 partial class Maze {
 
-    public class Node {
-        public char c;
+    public class Node { }
 
-        public Node(char c) => this.c = c;
-    }
+    /// <summary>Empty cell char.</summary>
+    const char EMPTY = ' ';
+    /// <summary>Wall char.</summary>
+    const char WALL = '█';
 
+    /// <summary>Maze height.</summary>
     public int m;
-
+    /// <summary>Maze width.</summary>
     public int n;
 
-    public Node[,] board;
+    /// <summary>Board matrix representing maze arrangement.</summary>
+    public char[,] board;
+
+    /// <summary>
+    /// Clears console and draws base maze (no empty cells).
+    /// </summary>
+    public void DrawBase() {
+        Console.Clear();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                Console.Write(WALL);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    /// <summary>
+    /// Draws char <c>c</c> on the screen position of cell <c>(i, j)</c>.
+    /// </summary>
+    public static void DrawOnCell(char c, int i, int j) {
+        const char ESC_CODE = (char) 0x1B;
+        Console.Write("{0}[{1};{2}H", ESC_CODE, i+1, j+1);
+        Console.Write(c);
+        Console.Write("{0}[{1};{2}H", ESC_CODE, i+1, j+1);
+    }
 }
