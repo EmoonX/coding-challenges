8462df1d5a67d30095c9930c071b2e15cda2d945
EmoonX <gustavo.chicato@usp.br>
2022-03-30 18:27:40 -0300

    [08] Allow dash switch using arrow keys

--- 08-hangman/hangman.py
+++ 08-hangman/hangman.py
@@ -1,19 +1,57 @@
 import os
 import random
+from select import select
+import sys
+from time import sleep
+
+from pynput.keyboard import Key, Listener
 
 
-def show(word: str):
-    os.system("clear")
-    print()
+word: str
+
+pos: int = 0
+
+
+def draw():
+    '''Something.'''
+    underline = lambda s: '\033[4m%s\033[0m' % s
+    
+    sleep(0.001)
+    os.system('clear')
+    print(' [Hangman]\n')
     for letter in word:
-        print("  \033[4m %s \033[0m" % letter, end="")
-    print('\n')
+        print('  %s ' % underline(' %s ' % ' '), end='')
+    
+    i, j = 3, (4 + 6*pos)
+    print('\x1b[%d;%df' % (i, j), end='')
+    print(underline('A'), end='')
+    print('\x1b[%d;%df' % (i, j), end='', flush=True)
+
+
+def on_press(key: Key):
+    # print('%s pressed' % key)    
+    draw()
+
+
+def on_release(key: Key):
+    # print('%s released' % key)
+    global pos
+    if key == Key.left:
+        pos = max(pos - 1, 0)
+        draw()
+    elif key == Key.right:
+        pos = min(pos + 1, len(word) - 1)
+        draw()
+    return key != Key.esc
 
 
 with open('1-1000.txt') as words_file:
     data = words_file.read()
     words = data.split('\n')
-    
-word = random.choice(words)
-word = word.upper()
-show(word)
+    word = random.choice(words)
+    word = word.upper()
+
+draw()
+
+with Listener(on_press, on_release) as listener:
+    listener.join()
