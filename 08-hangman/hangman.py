b817ac8f56c438f8db638a2234f537c51b5a31b6
EmoonX <gustavo.chicato@usp.br>
2022-03-31 14:50:34 -0300

    [08] Allow inputting letters on dashes

--- 08-hangman/hangman.py
+++ 08-hangman/hangman.py
@@ -3,11 +3,16 @@ import random
 from select import select
 import sys
 from time import sleep
+from typing import Union
 
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import (
+    Key, KeyCode, Listener
+)
 
 
-word: str
+hidden_word: str
+
+cur_word: bytearray
 
 pos: int = 0
 
@@ -19,38 +24,39 @@ def draw():
     sleep(0.001)
     os.system('clear')
     print(' [Hangman]\n')
-    for letter in word:
-        print('  %s ' % underline(' %s ' % ' '), end='')
+    for byte in cur_word:
+        letter = chr(byte)
+        s = underline(' %s ' % letter)
+        print('  %s ' % s, end='')
     
     i, j = 3, (4 + 6*pos)
-    print('\x1b[%d;%df' % (i, j), end='')
-    print(underline('A'), end='')
-    print('\x1b[%d;%df' % (i, j), end='', flush=True)
+    print('\x1b[%d;%dH' % (i, j), end='', flush=True)
 
 
-def on_press(key: Key):
+def on_press(key: Union[Key, KeyCode]):
     # print('%s pressed' % key)    
     draw()
 
 
-def on_release(key: Key):
+def on_release(key: Union[Key, KeyCode]):
     # print('%s released' % key)
     global pos
     if key == Key.left:
         pos = max(pos - 1, 0)
-        draw()
     elif key == Key.right:
-        pos = min(pos + 1, len(word) - 1)
-        draw()
+        pos = min(pos + 1, len(hidden_word) - 1)
+    elif isinstance(key, KeyCode) and key.char.isalpha():
+        cur_word[pos] = ord(key.char.upper())
+    draw()
     return key != Key.esc
 
 
 with open('1-1000.txt') as words_file:
     data = words_file.read()
     words = data.split('\n')
-    word = random.choice(words)
-    word = word.upper()
+    hidden_word = random.choice(words).upper()
 
+cur_word = bytearray(' ' * len(hidden_word), 'ascii')
 draw()
 
 with Listener(on_press, on_release) as listener:
