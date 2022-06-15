8e032bcd888c5611a5d536339ff2b982e386584a
EmoonX <gustavo.chicato@usp.br>
2022-06-15 19:07:32 -0300

    [02] Fix scale convertion formula

--- 02-temperature-converter/Temperature.cs
+++ 02-temperature-converter/Temperature.cs
@@ -2,14 +2,15 @@ Scale celsius = new("°C", 1, 0);
 Scale fahrenheit = new("°F", 1.8, 32);
 Scale kelvin = new("K", 1, 273.15);
 
-Temperature tempCelsius = new(celsius, -40);
-Temperature tempFahrenheit = tempCelsius.ToScale(fahrenheit);
-Temperature tempKelvin = tempCelsius.ToScale(kelvin);
+Temperature tempKelvin = new(kelvin, 0);
+Temperature tempCelsius = tempKelvin.ToScale(celsius);
+Temperature tempFahrenheit = tempKelvin.ToScale(fahrenheit);
 
 Console.WriteLine(tempCelsius);
 Console.WriteLine(tempFahrenheit);
 Console.WriteLine(tempKelvin);
 
+/// <summary>A.</summary>
 class Scale {
     public readonly string symbol;
 
@@ -29,9 +30,10 @@ class Temperature {
     public Temperature(Scale scale, double value)
         => (this.scale, this.value) = (scale, value);
 
-    public Temperature ToScale(Scale other) {
-        double value = (other.a * this.value) + other.b;
-        return new(other, value);
+    public Temperature ToScale(Scale target) {
+        double tempCelsius = (value - scale.b) / scale.a;
+        double tempTarget = (tempCelsius * target.a) + target.b;
+        return new(target, tempTarget);
     }
 
     public override string ToString()
