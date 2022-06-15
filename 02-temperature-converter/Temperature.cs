415f0741ba3a216746492f4af2fcf2fc35322a63
EmoonX <gustavo.chicato@usp.br>
2022-06-15 14:15:11 -0300

    [02] Conversions to Fahrenheit and Kelvin

--- 02-temperature-converter/Temperature.cs
+++ 02-temperature-converter/Temperature.cs
@@ -1,16 +1,44 @@
-Celsius celsius = new(14);
+Celsius celsius = new(-40);
+Fahrenheit fahrenheit = celsius.ToFahrenheit();
+Kelvin kelvin = celsius.ToKelvin();
 Console.WriteLine(celsius);
+Console.WriteLine(fahrenheit);
+Console.WriteLine(kelvin);
 
 class Temperature {
     protected double value;
 
-    public Temperature(double value)
+    protected Temperature(double value)
         => this.value = value;
 }
 
 class Celsius : Temperature {
     public Celsius(double value) : base(value) {}
 
+    public Fahrenheit ToFahrenheit() {
+        double value = (this.value * 1.8) + 32;
+        return new(value);
+    }
+
+    public Kelvin ToKelvin() {
+        double value = this.value + 273.15;
+        return new(value);
+    }
+
     public override string ToString()
-        => String.Format("{0} °C", value);
+        => String.Format("{0:.0} °C", value);
+}
+
+class Fahrenheit : Temperature {
+    public Fahrenheit(double value) : base(value) {}
+
+    public override string ToString()
+        => String.Format("{0:.0} °F", value);
+}
+
+class Kelvin : Temperature {
+    public Kelvin(double value) : base(value) {}
+
+    public override string ToString()
+        => String.Format("{0:.0} K", value);
 }
