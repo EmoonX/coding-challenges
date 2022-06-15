c8d1060db8be49a1c1269c4ade442410f3c7bd5d
EmoonX <gustavo.chicato@usp.br>
2022-06-15 01:27:59 -0300

    [26] Small style/convention changes

--- 26-radix-converter/radix.c
+++ 26-radix-converter/radix.c
@@ -4,14 +4,14 @@
 #include <string.h>
 
 /** All 36 available digits. */
-const char *digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+const char digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
 
 /**
  * Show program usage.
  */
-void show_usage(char *program_name) {
+void show_usage(char program_name[]) {
     printf("Usage: %s <number> <base_from> <base_to>\n\n", program_name);
-    puts("• number\n    An integer or floating point number.");
+    puts("• number\n    An integer number.");
     puts("• base_from, base_to\n    Integer bases from 2 to 36.");
     puts("");
 }
@@ -20,30 +20,31 @@ void show_usage(char *program_name) {
  * Convert string from given base/radix to equivalent one
  * in another base.
  */
-void convert(char *x, char *y, int base_from, int base_to) {
-    long long integer = 0;
-    if (x[0] == '-') {
+void convert(const char input[], char output[], int base_from, int base_to) {
+    const char *x = input;
+    char *y = output;
+    if (input[0] == '-') {
         // Easy hack for negative numbers
-        y[0] = '-';
+        output[0] = '-';
         x++;
         y++;
     }
-    // Get base 10 number from integer part
+    // Get base 10 number from input
+    long long decimal = 0;
     for (int i = 0; x[i] != '\0'; i++) {
         int k = strchr(digits, x[i]) - digits;
-        integer = (integer * base_from) + k;
+        decimal = (decimal * base_from) + k;
     }
-    // Build string in target base
-    int i;
+    // Build string in output's base and reverse it
     char aux[128];
-    for (i = 0; integer > 0; i++) {
-        int k = integer % base_to;
+    int i;
+    for (i = 0; decimal > 0; i++) {
+        int k = decimal % base_to;
         char c = digits[k];
         aux[i] = c;
-        integer /= base_to;
+        decimal /= base_to;
     }
-    // Reverse string for final result
-    int n = i;
+    size_t n = i;
     y[i] = '\0';
     for (i--; i >= 0; i--) {
         int j = n - i - 1;
@@ -57,12 +58,12 @@ int main(int argc, char **argv) {
         show_usage(argv[0]);
         return EXIT_FAILURE;
     }
-    char *x = argv[1];
+    char *input = argv[1];
     int base_from = atoi(argv[2]);
     int base_to = atoi(argv[3]);
 
     // Convert number and print result
-    char y[128];
-    convert(x, y, base_from, base_to);
-    printf("%s (%d) = %s (%d)\n", x, base_from, y, base_to);
+    char output[128];
+    convert(input, output, base_from, base_to);
+    printf("%s (%d) = %s (%d)\n", input, base_from, output, base_to);
 }
