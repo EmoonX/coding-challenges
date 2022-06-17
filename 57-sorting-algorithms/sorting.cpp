fe422bf3e74d189c723f1b1365d0138ebad317a8
EmoonX <gustavo.chicato@usp.br>
2022-06-17 13:48:57 -0300

    [57] Further documentation and CLI usage

--- 57-sorting-algorithms/sorting.cpp
+++ 57-sorting-algorithms/sorting.cpp
@@ -9,10 +9,15 @@
 
 using namespace std;
 
+/** How many values in list. */
 int n;
 
+/** The list to be sorted. */
 vector<int> list;
 
+/**
+ * Visually draws list as a graph on screen.
+ */
 void draw_list() {
     system("clear");
     for (int i = n; i >= 1; i--) {
@@ -26,6 +31,10 @@ void draw_list() {
     this_thread::sleep_for(chrono::milliseconds(20));
 }
 
+/**
+ * Swaps two values if x > y.
+ * Returns whether swap happened or not.
+ */
 bool check_and_swap(int& x, int& y) {
     if (x > y) {
         int aux = x;
@@ -36,6 +45,12 @@ bool check_and_swap(int& x, int& y) {
     return false;
 }
 
+/**
+ * Bubble sort, O(n²).
+ *
+ * Compares adjacent values and swaps them if out of order,
+ * moving the smallest value to the start of the unordered portion.
+ */
 void bubble_sort() {
     for (int i = 0; i < n-1; i++) {
         for (int j = n-1; j > i; j--) {
@@ -47,6 +62,12 @@ void bubble_sort() {
     }
 }
 
+/**
+ * Selection sort, O(n²).
+ *
+ * Compares first unordered element to all the others,
+ * moving a smaller one to the start whenever it's found.
+ */
 void selection_sort() {
     for (int i = 0; i < n-1; i++) {
         for (int j = i+1; j < n; j++) {
@@ -59,7 +80,22 @@ void selection_sort() {
 }
 
 int main(int argc, char **argv) {
+    // Map of availabe algorithms
+    map<string, void (*)()> algorithms = {
+        {"bubble", bubble_sort},
+        {"selection", selection_sort},
+    };
     // Receive arguments
+    if (argc != 3 or (not algorithms.count(argv[2]))) {
+        fprintf(stderr, "Usage: %s <size> <algorithm>\n\n", argv[0]);
+        fputs("• size\n    Size of the list to be sorted.\n\n", stderr);
+        fputs("• algorithm\n    Sorting algorithm to be used.\n    ( ", stderr);
+        for (auto& algo : algorithms) {
+            cerr << "\"" << algo.first << "\" ";
+        }
+        fprintf(stderr, ")\n\n");
+        return EXIT_FAILURE;
+    }
     n = atoi(argv[1]);
     string algorithm = argv[2];
 
@@ -73,12 +109,6 @@ int main(int argc, char **argv) {
     draw_list();
 
     // Get function pointer from string and call it
-    map<string, void (*)()> algorithms = {
-        {"bubble", bubble_sort},
-        {"selection", selection_sort},
-    };
-    if (algorithms.count(algorithm)) {
-        auto sort_function = algorithms[algorithm];
-        sort_function();
-    }
+    auto sort_function = algorithms[algorithm];
+    sort_function();
 }
