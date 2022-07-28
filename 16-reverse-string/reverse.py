98ca151fcd794ba87abd9d1f5a237b077ba8f979
EmoonX <gustavo.chicato@usp.br>
2022-07-28 12:51:01 -0300

    [16] Better Python wording

--- 16-reverse-string/reverse.py
+++ 16-reverse-string/reverse.py
@@ -1,5 +1,5 @@
 # `reversed(s)` is more efficient than `s[::-1] due to
-# returning an iterator (and not creating a whole new list)
+# returning an iterator (instead of creating a whole new list)
 s = input()
 for c in reversed(s):
     print(c, end='')
