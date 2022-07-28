c31d6287410409d04d098ada3d2b431419430a5f
EmoonX <gustavo.chicato@usp.br>
2022-07-28 13:27:44 -0300

    [16] C: UTF-8 multi-byte char support

--- 16-reverse-string/reverse.c
+++ 16-reverse-string/reverse.c
@@ -12,7 +12,19 @@ int main() {
         }
     }
     for (p--; p >= s; p--) {
-        putchar(*p);
+        if (*p & 0x80) {
+            // UTF-8 multi-byte char support
+            char *q = p - 1;
+            while (! (*q & 0x40)) {
+                q--;
+            }
+            for (char *r = q; r <= p; r++) {
+                putchar(*r);
+            }
+            p = q;
+        } else {
+            putchar(*p);
+        }
     }
     puts("");
 }
