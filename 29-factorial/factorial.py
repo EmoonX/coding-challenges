94a0d1ed863e66f60d8db63e22b562f700a04624
EmoonX <gustavo.chicato@usp.br>
2022-07-29 12:21:16 -0300

    [29] Simple factorial

---
+++ 29-factorial/factorial.py
@@ -0,0 +1,11 @@
+import sys
+
+def factorial(n: int) -> int:
+    '''Returns the factorial of `n`.'''
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+n = int(sys.argv[1])
+ans = factorial(n)
+print(f"{n}! = {ans}")
