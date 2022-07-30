9cc50c967afafa798a29ff0a5eb37668ad75006c
EmoonX <gustavo.chicato@usp.br>
2022-07-30 08:35:26 -0300

    [55] RPN: Build stack from tokens

--- 55-reverse-polish-notation/postfix.kt
+++ 55-reverse-polish-notation/postfix.kt
@@ -1,5 +1,24 @@
-fun calculate(postfix: String): String {
-    return postfix
+class Stack {
+
+    val tokens = ArrayDeque<String>()
+
+    fun push(value: String) {
+        tokens.addLast(value)
+    }
+
+    fun pop(): String {
+        val last = tokens.removeLast()
+        return last
+    }
+}
+
+fun calculate(postfix: String): String? {
+    val tokens = postfix.split(" ")
+    val stack = Stack()
+    for (token in tokens) {
+        stack.push(token)
+    }
+    return stack.pop()
 }
 
 fun main() {
@@ -8,6 +27,8 @@ fun main() {
         print(">>> ")
         postfix = readLine()!!
         val result = calculate(postfix)
-        println(result)
+        if (result != "") {
+            println(result)
+        }
     }
 }
