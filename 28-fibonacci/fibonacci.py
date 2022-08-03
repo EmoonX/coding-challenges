e57832947b892178181dfd8843b11185ee773ef1
EmoonX <gustavo.chicato@usp.br>
2022-08-03 01:31:47 -0300

    [28] `@cache` decorator for clean memoization

--- 28-fibonacci/fibonacci.py
+++ 28-fibonacci/fibonacci.py
@@ -1,14 +1,13 @@
+from functools import cache
 import sys
 
-# Dict of F(n) fibonacci values
-fib = {0: 0, 1: 1}
-
+@cache
 def fibonacci(n: int) -> int:
-    '''Returns value of `F(n)`, using memoization
-    to keep track of previously calculated values.'''
-    if not n in fib:
-        fib[n] = fibonacci(n-1) + fibonacci(n-2)
-    return fib[n]
+    '''Returns value of `F(n)`.
+    Uses `@cache` decorator for quick and clean memoization.'''
+    if n in (0, 1):
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
 
 n = int(sys.argv[1])
 ans = fibonacci(n)
