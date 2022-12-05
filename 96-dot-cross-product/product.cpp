913e4aa43089b554f21f1aa54d98a06dd8fffcfd
EmoonX <emoon@emoon.dev>
2022-12-05 10:07:59 -0300

    [96] Dot product

--- 96-dot-cross-product/product.cpp
+++ 96-dot-cross-product/product.cpp
@@ -4,14 +4,17 @@
 
 using namespace std;
 
-/** N-dimensional vector. */
+/** `n`-dimensional vector. */
 class Vector {
 private:
-    /** Vector name. */
-    string name;
+    /** Vector's name. */
+    const string name;
 
     /** Vector's numerical components. */
-    vector<double> v;
+    vector<double> vec;
+
+    /** Vector's dimension (i.e number of components) */
+    size_t n;
 
 public:
     /** Builds named vector from input values. */
@@ -22,19 +25,35 @@ public:
             if (input[r] == ' ' or r == input.size()) {
                 string s = input.substr(l, r - l);
                 double x = stod(s);
-                v.push_back(x);
+                vec.push_back(x);
                 l = r + 1;
             }
         }
+        n = vec.size();
+    }
+
+    /** Gets `i`-th vector component. */
+    double operator[](size_t i) const {
+        return vec.at(i);
+    }
+
+    /** Calculates dot product with another vector. */
+    double dot(const Vector &v) const {
+        const Vector &u = *this;
+        double ans = 0.0;
+        for (size_t i = 0; i < n; i++) {
+            ans += u[i] * v[i];
+        }
+        return ans;
     }
     
     /** Prints vector representation. */
-    void print() {
-        cout << name << ": ( ";
-        for (double x : v) {
+    void print() const {
+        cout << name << ": [ ";
+        for (double x : vec) {
             cout << x << " ";
         }
-        cout << ")" << endl;
+        cout << "]" << endl;
     }
 };
 
@@ -46,11 +65,12 @@ Vector read_vector(const string &name) {
 }
 
 int main() {
-    // Something
+    // Receive inputs
     Vector u = read_vector("u");
     Vector v = read_vector("v");
     
-    // More things
+    // Print results
     u.print();
     v.print();
+    cout << "u . v = " << u.dot(v) << endl;
 }
