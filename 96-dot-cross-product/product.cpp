ed505972a8a68336b9f0858c650b1ab0979e5551
EmoonX <emoon@emoon.dev>
2022-12-05 11:17:08 -0300

    [96] Cross product

--- 96-dot-cross-product/product.cpp
+++ 96-dot-cross-product/product.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -32,24 +33,48 @@ public:
         n = vec.size();
     }
 
-    /** Gets `i`-th vector component. */
+    /** Returns `i`-th vector component. */
     double operator[](size_t i) const {
         return vec.at(i);
     }
+    /** Returns `i`-th vector component reference (for lvalue usage). */
+    double & operator[](size_t i) {
+        return vec.at(i);
+    }
 
-    /** Calculates dot product with another vector. */
-    double dot(const Vector &v) const {
+    /** Calculates dot product between two vectors. Returns a scalar. */
+    constexpr double dot(const Vector &v) const {
         const Vector &u = *this;
-        double ans = 0.0;
+        double z = 0.0;
         for (size_t i = 0; i < n; i++) {
-            ans += u[i] * v[i];
+            z += u[i] * v[i];
         }
-        return ans;
+        return z;
+    }
+
+    /** Calculates cross product between two vectors. 
+        Returns a 3-dimensional vector. */
+    Vector cross(const Vector &v) const {
+        static vector<size_t> perm = {0, 1, 2};
+        const Vector &u = *this;
+        string name = u.name + " x " + v.name;
+        Vector w(name, "0 0 0");
+        do {            
+            int inversion_count = 0;
+            for (size_t k = 1; k <= 3; k++) {
+                if (perm[k-1] >= k) {
+                    inversion_count++;
+                }
+            }
+            int factor = (inversion_count % 2 == 0) ? 1 : -1;
+            w[perm[0]] = factor * u[perm[1]] + v[perm[2]];
+        } while (next_permutation(perm.begin(), perm.end()));
+        return w;
     }
     
     /** Prints vector representation. */
     void print() const {
-        cout << name << ": [ ";
+        cout << name << " = [ ";
         for (double x : vec) {
             cout << x << " ";
         }
@@ -69,8 +94,9 @@ int main() {
     Vector u = read_vector("u");
     Vector v = read_vector("v");
     
-    // Print results
+    // Calculate and print results
     u.print();
     v.print();
     cout << "u . v = " << u.dot(v) << endl;
+    (u.cross(v)).print();
 }
