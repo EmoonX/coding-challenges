a4379151dcf8ae4aaf21d19c1606092488454ee0
EmoonX <gustavo.chicato@usp.br>
2022-12-07 11:16:49 -0300

    [77] Good old documentation

--- 77-brainfuck/brainfuck.rs
+++ 77-brainfuck/brainfuck.rs
@@ -1,15 +1,20 @@
 use std::io::{BufRead, stdin, stdout, Write};
 
+/// Size of cell array.
 const N: usize = 30_000;
 
+/// The Brainfuck machine.
 struct Machine {
+    /// Data cells as an array of bytes.
     cells: [u8; N],
-    ptr: *mut u8, 
+    /// Movable data pointer.
+    ptr: *mut u8,
+    /// Program defined as a sequence of single characters (`><+-.,[]`).
     program: String,
 }
 
 impl Machine {
-
+    /// Creates a new `Machine` with given input program.
     fn new(input: String) -> Self {
         let mut cells = [0; N];
         Self {
@@ -19,12 +24,14 @@ impl Machine {
         }
     }
 
+    /// Runs machine, iterating through commands.
     fn run(&self) {
         for c in self.program.chars() {
             self.parse(c);
         }
     }
 
+    /// Parses single command.
     fn parse(&self, command: char) {
         match command {
             '>' => self.increment_pointer(),
@@ -38,35 +45,50 @@ impl Machine {
             _ => (),
         }
     }
+    
+    // The following method descriptions were acquired from Wikipedia.
 
+    /// Increments the data pointer (to point to the next cell to the right).
     fn increment_pointer(&self) {
         println!(">: Incrementing pointer");
     }
 
+    /// Decrements the data pointer (to point to the next cell to the left).
     fn decrement_pointer(&self) {
         println!("<: Decrementing pointer");
     }
 
+    /// Increments (increases by one) the byte at the data pointer.
     fn increment_byte(&self) {
         println!("+: Incrementing byte");
     }
 
+    /// Decrements (decreases by one) the byte at the data pointer.
     fn decrement_byte(&self) {
         println!("-: Decrementing byte");
     }
 
+    /// Outputs the byte at the data pointer. 
     fn output(&self) {
         println!(".: Outputting");
     }
 
+    /// Accepts one byte of input,
+    /// storing its value in the byte at the data pointer.
     fn input(&self) {
         println!(",: Waiting for input...");
     }
 
+    /// If the byte at the data pointer is zero, then
+    /// instead of moving the instruction pointer forward to the next command,
+    /// jumps it forward to the command after the matching `]` command. 
     fn open_loop(&self) {
         println!("[: Opening loop")
     }
 
+    /// If the byte at the data pointer is nonzero, then
+    /// instead of moving the instruction pointer forward to the next command,
+    /// jumps it back to the command after the matching `[` command.
     fn close_loop(&self) {
         println!("]: Closing loop")
     }
