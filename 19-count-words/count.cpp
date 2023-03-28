655318adc8f33bf95fe2383b2bc004b3aa28982b
EmoonX <gustavo.chicato@usp.br>
2023-03-28 18:26:40 -0300

    [19] Word counter

--- 19-count-words/count.cpp
+++ 19-count-words/count.cpp
@@ -7,30 +7,57 @@ using std::cout, std::endl;
 
 struct CountableString {
 private:
-    /** String data representation. */
+    /** String's content. */
     std::string data;
 
     /** View pointing to last read string line. */
     std::string_view current_line;
 
+    /** Token counters. */
+    struct {
+        uint words;
+    } count;
+
 public:
     /** Appends line to string, bar special character case. */
     CountableString & operator+=(const std::string &line) {
-        if (line[0] != '@') {
+        if (line[0] == '@') {
+            show_count();
+        } else {
             data += line;
             current_line = line;
+            update_count();
         }
         return *this;
     }
 
+    /** Update counters. */
+    void update_count() {
+        bool in_word = false;
+        for (auto &c : current_line) {
+            if (isgraph(c)) {
+                if (not in_word) {
+                    in_word = true;
+                    count.words++;
+                }
+            } else {
+                in_word = false;
+            }
+        }
+    }
+
     /** Prints whole string. */
-    void print() {
+    void print() const {
         cout << data << endl;
     }
 
-    /** Prints current string line. */
-    void print_line() {
+    /** Prints string's current line. */
+    void print_line() const {
         cout << current_line << endl;
+    }
+
+    void show_count() const {
+        cout << "> Words: " << count.words << endl;
     }
 };
 
@@ -40,5 +67,5 @@ int main() {
     getline(cin, aux);
     cs += aux;
     cs.print();
-    cs.print_line();
+    cs.show_count();
 }
