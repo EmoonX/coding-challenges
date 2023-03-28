3906921395c254fb8a3da93552beedf5dc8b1243
EmoonX <gustavo.chicato@usp.br>
2023-03-28 16:09:53 -0300

    [19] `print()` method and docs

--- 19-count-words/count.cpp
+++ 19-count-words/count.cpp
@@ -5,30 +5,40 @@
 using std::cin;
 using std::cout, std::endl;
 
-struct CountableString
-{
+struct CountableString {
+private:
+    /** String data representation. */
     std::string data;
-    std::string_view line;
 
-    CountableString & operator+=(const std::string &aux) {
-        if (aux[0] != '@') {
-            data += aux;
-            line = aux;
+    /** View pointing to last read string line. */
+    std::string_view current_line;
+
+public:
+    /** Appends line to string, bar special character case. */
+    CountableString & operator+=(const std::string &line) {
+        if (line[0] != '@') {
+            data += line;
+            current_line = line;
         }
         return *this;
     }
 
+    /** Prints whole string. */
+    void print() {
+        cout << data << endl;
+    }
+
+    /** Prints current string line. */
     void print_line() {
-        cout << line << endl;
+        cout << current_line << endl;
     }
 };
 
-
-int main()
-{
+int main() {
     CountableString cs;
     std::string aux;
     getline(cin, aux);
     cs += aux;
+    cs.print();
     cs.print_line();
 }
