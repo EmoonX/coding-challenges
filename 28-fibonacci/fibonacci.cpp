4ab644e00dc7387771c9ea188993cbc24bd7ea83
EmoonX <gustavo.chicato@usp.br>
2024-03-01 11:53:27 -0300

    [28] Better input checking and `std::optional`

--- 28-fibonacci/fibonacci.cpp
+++ 28-fibonacci/fibonacci.cpp
@@ -5,22 +5,24 @@
 
 /** Calculates the `n`th Fibonacci number */
 int64_t fibonacci(int n) {
-    static std::vector<int64_t> fib(n+1, -1);
-    if (fib[n] != -1) {
-        return fib[n];
+    static std::vector<std::optional<int64_t>> fib(n+1);
+    if (fib[n]) {
+        return *fib[n];
     }
     if (n == 0 or n == 1) {
         return n;
     }
-    fib[n] = fibonacci(n-2) + fibonacci(n-1);
-    return fib[n];
+    fib[n] = fibonacci(n-1) + fibonacci(n-2);
+    return *fib[n];
 }
 
 int main(int argc, char* argv[]) {
     int n;
     try {
-        n = std::stoi(argv[1]);
-        if (n < 0 or n > 90) {
+        // Fool-proof(-ish) way of reading an integer in a valid range
+        size_t pos;
+        n = std::stoi(argv[1], &pos);
+        if (argv[1][pos] != '\0' or n < 0 or n > 90) {
             throw std::invalid_argument("");
         }
     } catch (...) {
