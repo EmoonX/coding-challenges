df4dcf1ced4e348da80d2cc1a86b868d52069a24
EmoonX <gustavo.chicato@usp.br>
2024-03-02 17:29:59 -0300

    [68] C++, extra touches

--- 67-prime-numbers/primes.cpp
+++ 67-prime-numbers/primes.cpp
@@ -4,6 +4,7 @@
 
 #include "../util/print.hpp"
 
+/** Generates the next prime by using the sieve of Eratosthenes. */
 int generate_next_prime() {
     static std::array<bool, 123'456'789> sieve;
     static int k = 2;
@@ -20,16 +21,14 @@ int generate_next_prime() {
 
 int main(int argc, char* argv[]) {
     int n = std::stoi(argv[1]);
-    FANCY_PRINT("The first {} prime numbers", n);
-    PRINT("\n");
+    FANCY_PRINT("{}\n", "The first {} prime numbers", n);
     for (int count = 1; count <= n; count++) {
         if (count == 1 or count % 10 == 0) {
-            PRINT("\n");
-            FANCY_PRINT("{:3d}", count);
+            FANCY_PRINT("\n{}", "{:3d}", count);
         }
         auto prime = generate_next_prime();
-        std::cout << prime << " ";
+        PRINT("{} ", prime);
     }
-    std::cout << std::endl;
+    PRINTLN();
 }
 
